616b3fc94518cdee6c7f28fafb19e6d958442414
Niklas Netter <nettern@students.uni-marburg.de>
2018-12-25 17:24:03 +0100

    Eigenfaces is complete. Arbitrary portraits can be maped, too.

--- eigenfaces.jl
+++ eigenfaces.jl
@@ -16,9 +16,7 @@ function get_folder(folder::AbstractString, number::Int, resize_y::Int=120, resi
     pic_matrix
 end
 
-function get_matrix(folderlist)
-    resize_x = 120
-    resize_y = 80
+function get_matrix(folderlist, resize_y::Int=120, resize_x::Int=80)
 
     total_num = sum(map(x->x[2], folderlist))[1]
     matrix = zeros(Gray{Float64}, total_num, resize_y*resize_x)
@@ -38,14 +36,83 @@ function normal(A)
     (A .- min) / (max .- min)
 end
 
+function normal_list!(A)
+    for i = 1:size(A)[2]
+        A[:, i] = normal(A[:, i])
+    end
+end
+
+function get_scalars(vectors, A)
+    num = size(vectors)[2]
+
+    list = zeros(eltype(vectors[:, 1]), num)
+    for i = 1:num
+        list[i] = sum(vectors[:, i] .* A)
+    end
+    list
+end
+
+function get_A_from_scalars(scalars, vectors)
+    B = zeros(eltype(vectors[:, 1]), size(vectors[:, 1]))
+    for i = 1:size(scalars)[1]
+        B = B .+ vectors[:, i] .* scalars[i]
+    end
+    B
+end
+
+function plot_scalars(imgs, vectors, k)
+    num_imgs = size(imgs)[1]
+    num_vecs = size(vectors)[2]
+    scalars = zeros(eltype(vectors[:, 1]), num_imgs, num_vecs)
+    for i = 1:num_imgs
+        scalars[i, :] = get_scalars(vectors, imgs[i])
+    end
+
+    display(plot(k, scalars[1, :]))
+    for i = 2:num_imgs
+        display(plot!(k, scalars[i, :]))
+    end
+
+    scalars
+end
+
+function compare_img(vectors, imgs, scalars, number::Int, resize_y::Int=120, resize_x::Int=80)
+    compare = zeros(eltype(imgs[1]), (resize_y, resize_x*2)) # a bigger picture
+    compare[:, 1:resize_x] = get_A_from_scalars(scalars[number, :], vectors) # calculate pic only from k vectors
+    compare[:, resize_x+1:end] = imgs[number] # the original
+    compare # you dont see much differences
+end
+
+function compare_all_imgs(vectors, imgs, scalars, number::Int, resize_y::Int=120, resize_x::Int=80)
+    num_imgs = size(imgs)[1]
+    compare = zeros(eltype(imgs[1]), (num_imgs*resize_y, resize_x*2)) # a big picture
+    for i = 1:num_imgs
+        compare[(i-1)*resize_y+1:i*resize_y, :] = compare_img(vectors, imgs, scalars, i, resize_y, resize_x)
+    end
+
+    compare
+end
+
+resize_x = 80
+resize_y = 120
+
 mean(get_folder("clooney", 5), dims=1)[1, :, :] # get a mean face
-B = get_matrix([["clooney", 5], ["cooper", 5], ["lawrence", 5], ["stone", 5]]);
+
+B = get_matrix([["clooney", 5], ["cooper", 5], ["lawrence", 5], ["stone", 5]], resize_y, resize_x);
 C = B' * B # calculate the correlation matrix
 E = eigen(C) # get the Eigenvalues and Eigenvectors and save it in E, too play around
-Gray.(reshape(normal(E.vectors[:, 9595]), (80, 120))) # look at an eigenface
-k = 9580:9600
-gr()
-plot(k, E.values[k], yaxis=:log) # plot the highest eigenvalues
+Gray.(reshape(normal(E.vectors[:, 9586]), (resize_y, resize_x))) # look at an eigenface
 
+k = 5571:9600 # look only at the highest eigenfaces
+gr()
+
+vectors = E.vectors[:, k] # view for highest eigenfaces
+
+V = get_matrix([["von_neumann", 2], ["mcconaughey", 1]], resize_y, resize_x);
+
+imgs = [B[16, :], B[17, :],  B[1, :], V[1, :], V[2, :], V[3, :]] # array of images
+scalars = plot_scalars(imgs, vectors, k) # plot and get the scalars of imgs
+
+compare_all_imgs(vectors, imgs, scalars, 6, resize_y, resize_x)
 
 end  # modul eigenfaces
